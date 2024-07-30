d5b52c77d7b7eb3c28b3f96baeefc7fb928a3141
Dangersaadi <154743025+Dangersaadi@users.noreply.github.com>
2024-07-30 13:43:00 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+// import React, { useState } from 'react';
+// import { Input, Button, List, Avatar, Space } from 'antd';
+
+// const TodoList = () => {
+//   const [todos, setTodos] = useState([]);
+
+//   const handleAddTodo = (value) => {
+//     setTodos([...todos, { text: value }]);
+//   };
+
+//   return (
+//     <Space direction="vertical">
+//       <Input.Search
+//         placeholder="Add Todo"
+//         enterButton="Add"
+//         onSearch={handleAddTodo}
+//       />
+//       <List
+//         dataSource={todos}
+//         renderItem={(item) => (
+//           <List.Item>
+//             <List.Item.Meta
+//               avatar={<Avatar>{item.text[0]}</Avatar>}
+//               title={item.text}
+//             />
+//           </List.Item>
+//         )}
+//       />
+//     </Space>
+//   );
+// };
+
+// export default TodoList;
+
+import React, {useState} from "react";
+import { Input, Button, List, Avatar, Space } from "antd";
+import Item from "antd/es/list/Item";
+const TodoList = () => {
+  const[todos, setTodos]=useState([]);
+  const handleAddTodo = (value) => {setTodos([...todos,{text:Avatar}]); }
+  ;
+  return(
+    <Space direction="vertical">
+      <Input.Search
+      placeholder="Add Todo"
+      enterButton="Add"
+      onSearch={handleAddTodo}
+      />
+ 
+ <List
+    itemLayout="horizontal"
+    dataSource={todos}
+    renderItem={(item, index) => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
+          title={<a href="https://ant.design">text</a>}
+         
+        />
+      </List.Item>
+    )}
+  />
+
+      </Space>
   );
-}
-
-export default App;
+};
+export default TodoList;
